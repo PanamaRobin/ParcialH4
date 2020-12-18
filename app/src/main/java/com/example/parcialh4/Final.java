572e5ef897288dd5e8af5d07fc8fc99268342c7b
Jose Luis <villarrealjoseluis@outlook.com>
2020-12-17 23:23:14 -0500

    Final

--- app/src/main/java/com/example/parcialh4/Final.java
+++ app/src/main/java/com/example/parcialh4/Final.java
@@ -4,17 +4,18 @@ package com.example.parcialh4;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Final extends AppCompatActivity{
 
-    private TextView txtoportunidad;
-    private TextView txtpuntaje;
-    private TextView txtTiempo;
 
-    TextView personaje;
+    private Button inicio;
+
+
 
     public static String heroe;
     @Override
@@ -23,22 +24,27 @@ public class Final extends AppCompatActivity{
         setContentView(R.layout.termina);
 
 
-        txtoportunidad = findViewById(R.id.idpersonaje);
-        txtpuntaje = findViewById(R.id.idpuntaje);
-        txtTiempo = findViewById(R.id.idTiempo);
-
-        personaje = findViewById(R.id.idpersonaje);
+        inicio=findViewById(R.id.idinicio);
 
         //LLAMADA AL INTENT ENVIADO DESDE EL LEVEL3
         Intent intent = getIntent();
         heroe = intent.getStringExtra("heroe");
 
-        personaje.setText(heroe);
         BDSQLite admin= new BDSQLite(this,"referencias",null,1);
         SQLiteDatabase db=admin.getWritableDatabase();
 
-            db.delete("partidaguardada","personaje= '"+heroe+"'", null);
-            db.close();
-
+        db.delete("partidaguardada","personaje= '"+heroe+"'", null);
+        db.close();
+        inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirInicio();
+            }
+        });
+    }
+    public void abrirInicio()
+    {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
